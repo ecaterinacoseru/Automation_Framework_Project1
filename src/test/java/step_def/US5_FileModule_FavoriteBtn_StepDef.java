af2c4c28fda3bfd678400856fd7217503c3d7817
Ecaterina <ecaterinacoseru@gmail.com>
2022-11-19 14:41:53 -0500

    US5 added

---
+++ src/test/java/step_def/US5_FileModule_FavoriteBtn_StepDef.java
@@ -0,0 +1,30 @@
+package step_def;
+
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+import pages.Dashboard;
+import pages.Login;
+
+public class US5_FileModule_FavoriteBtn_StepDef {
+
+    private final Login login = new Login();
+    private final Dashboard dashboard = new Dashboard();
+
+    @When("the user clicks action-icon  from any file on the page")
+    public void the_user_clicks_action_icon_from_any_file_on_the_page() {
+
+    }
+    @When("user choose the {string} option")
+    public void user_choose_the_option(String string) {
+
+    }
+    @When("user click the {string} sub-module on the left side")
+    public void user_click_the_sub_module_on_the_left_side(String string) {
+
+    }
+    @Then("Verify the chosen file is listed on the table")
+    public void verify_the_chosen_file_is_listed_on_the_table() {
+
+    }
+
+}
