6d01da93dcc535678614624fa756867dad49051c
Yevhen Marushchak <maruschak1992@gmail.com>
2022-11-18 23:21:40 -0500

    Updated

--- src/test/java/step_def/US2_Login_invalidCredentials_StepDef.java
+++ src/test/java/step_def/US2_Login_invalidCredentials_StepDef.java
@@ -4,28 +4,29 @@ import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
 import pages.Login;
+import utilities.BrowserUtils;
 
 public class US2_Login_invalidCredentials_StepDef {
 
     private final Login login = new Login();
 
-    @Given("user on the login page")
-    public void user_on_the_login_page() {
-
-    }
+//    @Given("user on the login page")
+//    public void user_on_the_login_page() {
+//        login.goLoginPage();
+//    }
 
     @When("user enter invalid {string} and {string}")
-    public void user_enter_invalid_and(String string, String string2) {
-
+    public void user_enter_invalid_and(String username, String password) {
+        login.login(username, password);
     }
 
     @When("user click the login button")
     public void user_click_the_login_button() {
-
+        login.loginBtnClick();
     }
 
     @Then("verify {string} message should be displayed")
-    public void verify_message_should_be_displayed(String string) {
-
+    public void verify_message_should_be_displayed(String errorMessage) {
+        login.errorMessageVerification(errorMessage);
     }
 }
